3be5ba9a65736bef37dadaac66d315cbd76617e6
redfr0g <redfr0g@protonmail.com>
2021-04-27 21:02:53 +0200

    Script simplification

--- ssrfuzz.sh
+++ ssrfuzz.sh
@@ -14,23 +14,13 @@ collaborator=$2
 wordlist=$3
 output="$domain-$(date +'%m-%d-%Y').txt"
 
-#Find known URLs and grep these with parameters
+# Find known URLs and grep these with parameters
 echo "[+] Finding URLs for domain $domain"
-gau -b ttf,woff,svg,png,jpg,jpeg,css $domain | grep "=" | sort -u | grep "?" > $output
+gau -b ttf,woff,svg,png,jpg,jpeg,css -subs $domain | grep "=" | sort -u | grep "?" > $output
 echo "[+] Found $(wc -l < $output) URLs"
 
-# Check for alive URLs
-echo "[+] Checking for active URLs"
-echo -n "" > fuzz.txt
-for url in $(cat $output)
-do
-  if curl -k -x http://127.0.0.1:8080 --output /dev/null --silent --fail "$url"; then
-    echo "$url" >> fuzz.txt
-  else
-    :
-  fi
-done
-echo "[+] Got $(wc -l < fuzz.txt) fuzzable URLs"
+# Copy results to temp file
+cp $output fuzz.txt
 
 # Fuzz with Collaborator link and custom URL parameters wordlist (i.e. from ParamMiner)
 echo "[+] Fuzzing parameters (this may take a while)..."
